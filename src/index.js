4fa623cd321441313596f49250bde14f94688c5a
Yumfii <144070576+Yumfii@users.noreply.github.com>
2024-09-12 16:47:18 +0200

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
+
+import { App } from './components/App.jsx';
+import 'modern-normalize';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
