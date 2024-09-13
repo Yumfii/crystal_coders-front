4dfa26d341edb8decf07642b552eb04e8490aaab
Maria Ensari <149113366+marotr@users.noreply.github.com>
2024-09-13 16:02:58 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,18 +1,27 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+
 
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <Routes>
+      <Route />
+
+
+    </Routes>
+
+
+    // <div
+    //   style={{
+    //     height: '100vh',
+    //     display: 'flex',
+    //     justifyContent: 'center',
+    //     alignItems: 'center',
+    //     fontSize: 40,
+    //     color: '#010101'
+    //   }}
+    // >
+    //   React homework template
+    // </div>
   );
 };
