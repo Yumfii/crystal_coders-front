69630bd3ba95a89c75a3beb7ffd55ce60f199aec
Yumfii <144070576+Yumfii@users.noreply.github.com>
2024-09-14 09:17:18 +0200

    root update and bugfix

--- src/index.js
+++ src/index.js
@@ -4,9 +4,12 @@ import ReactDOM from 'react-dom/client';
 import { App } from './components/App.jsx';
 import 'modern-normalize';
 import './index.css';
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter basename="/react-homework-template">
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
