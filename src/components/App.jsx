7efc37184b912209497e3cc1de59cf699c1fbdf8
Yumfii <144070576+Yumfii@users.noreply.github.com>
2024-09-14 09:27:12 +0200

    app update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,12 @@
+// import HomePage from 'pages/HomePage/HomePage';
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
 
 export const App = () => {
   return (
+    // <Routes>
+    //   <Route path="/" element = {<HomePage/>} />
+    // </Routes>
     <div
       style={{
         height: '100vh',
