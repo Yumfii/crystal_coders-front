42e4bc976d4755e630b8081abcee6386449919f9
Maria Ensari <149113366+marotr@users.noreply.github.com>
2024-09-14 15:18:42 +0300

    added images for advantages section

--- src/components/AdvantagesSection/AdvantagesSection.jsx
+++ src/components/AdvantagesSection/AdvantagesSection.jsx
@@ -1,40 +1,45 @@
-import { CloudinaryContext, Image } from "cloudinary-react";
-import css from "./AdvantagesSection.module.css";
-// import { Cloudinary } from "cloudinary-react";
+import { CloudinaryContext, Image } from 'cloudinary-react';
+import css from './AdvantagesSection.module.css';
+import { Cloudinary } from 'cloudinary-core';
 
 const AdvantatgesSection = () => {
-    return (
+  const cloudinary = new Cloudinary({
+    cloud_name: 'dwyxffoux',
+  });
 
+  return (
     <CloudinaryContext cloudName="dwyxffoux">
-    <div className={css.advantages}>
-      <div className={css.customersContainer}>
-        <ul className={css.customersList}>
-          <li className={css.customersImg}>
-            <Image publicId="tmbodlkkjpdiqirehfp6" alt="customer1" />
-          </li>
-          <li className={css.customersImg}>
-            <Image publicId="gn4nssaqqugodu3pnzi2" alt="customer2" />
-          </li>
-          <li className={css.customersImg}>
-            <Image publicId="og5jodgwvckh5kxgalyq" alt="customer3" />
-          </li>
-        </ul>
+      <div className={css.advantages}>
+        <div className={css.customersContainer}>
+          <ul className={css.customersList}>
+            <li className={css.customersImg}>
+              <Image publicId="tmbodlkkjpdiqirehfp6" alt="customer1" />
+            </li>
+            <li className={css.customersImg}>
+              <Image publicId="gn4nssaqqugodu3pnzi2" alt="customer2" />
+            </li>
+            <li className={css.customersImg}>
+              <Image publicId="og5jodgwvckh5kxgalyq" alt="customer3" />
+            </li>
+          </ul>
 
-        <p className={css.customersText}>
-          Our  <span className={css.happy}> 25 happy</span> customers
-        </p>
-      </div>
-      <div className={css.benefits}>
-      <div className={css.habitContainer}>
-        <div className={css.circle}></div>
-        <p className={css.habitText}>Habit drive</p>
+          <p className={css.customersText}>
+            Our <span className={css.happy}> 25 happy</span> customers
+          </p>
         </div>
-      <div className={css.statistics}><p className={css.statisticsText}>View statistics</p></div>
-      <div className={css.setting}>
-        <p className={css.settingText}>Personal rate setting</p>
+        <div className={css.benefits}>
+          <div className={css.habitContainer}>
+            <div className={css.circle}></div>
+            <p className={css.habitText}>Habit drive</p>
+          </div>
+          <div className={css.statistics}>
+            <p className={css.statisticsText}>View statistics</p>
+          </div>
+          <div className={css.setting}>
+            <p className={css.settingText}>Personal rate setting</p>
+          </div>
+        </div>
       </div>
-      </div>
-    </div>
     </CloudinaryContext>
   );
 };
