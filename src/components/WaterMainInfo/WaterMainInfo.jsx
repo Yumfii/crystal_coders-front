5dd942344278cdeb90a480c83b87b3b600a47275
Roman Vashchenko <roman1991dp@gmail.com>
2024-09-16 02:35:13 +0300

    update

--- src/components/WaterMainInfo/WaterMainInfo.jsx
+++ src/components/WaterMainInfo/WaterMainInfo.jsx
@@ -1,16 +1,19 @@
-import React from 'react';
 import css from './WaterMainInfo.module.css';
+import { GoPlus } from 'react-icons/go';
 
 const WaterMainInfo = () => {
+  const value = 50;
   return (
     <div className={css.section}>
       <div className={css.container}>
         <h1 className={css.title}>AquaTrack</h1>
+
         {/* WaterDailyNorma */}
         <div className={css.dailyNorma}>
           <span className={css.numberLiters}>1.5 L</span>
           <p className={css.text}>My daily norma</p>
         </div>
+
         {/* WaterProgressBar */}
         <div className={css.waterProgressBar}>
           <p className={css.day}>Today</p>
@@ -18,9 +21,11 @@ const WaterMainInfo = () => {
             type="range"
             min="0"
             max="100"
-            value="50"
-            readOnly
+            value={value}
             className={css.linearProgress}
+            style={{
+              background: `linear-gradient(to right, #9BE1A0  ${value}%, #F0EFF4 ${value}%)`,
+            }}
           />
           <div className={css.values}>
             <div>0%</div>
@@ -28,9 +33,11 @@ const WaterMainInfo = () => {
             <div>100%</div>
           </div>
         </div>
+
         {/* AddWaterBtn */}
         <div className={css.wrapper}>
           <button type="button" className={css.addWaterBtn}>
+            <GoPlus size={22} />
             Add water
           </button>
         </div>
