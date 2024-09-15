c3bd628d48ec70751c43a0c4266f81c1d6c2c8e6
Lev <leozolotukhin@gmail.com>
2024-09-15 16:04:52 +0200

    сsd

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,11 @@
-
 import HomePage from 'pages/HomePage/HomePage';
 import React from 'react';
-import {  Route, Routes } from 'react-router-dom';
-
-
+import { Route, Routes } from 'react-router-dom';
 
 export const App = () => {
   return (
     <Routes>
-      <Route path="/" element = {<HomePage/>} />
+      <Route path="/" element={<HomePage />} />
     </Routes>
     // <div
     //   style={{
@@ -22,6 +19,5 @@ export const App = () => {
     // >
     //   React homework template
     // </div>
-
   );
 };
