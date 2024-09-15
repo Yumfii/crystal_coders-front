851f1b78097ad5c28bb0a28fb8657dcce94387a0
Yilchik <yulyayulia2@gmail.com>
2024-09-15 09:33:23 +0200

    SignUpPage

---
+++ src/pages/SignUpPage/SignUpPage.jsx
@@ -0,0 +1,11 @@
+import SignUpForm from 'components/SignUpForm/SignUpForm';
+
+const SignUpPage = () => {
+  return (
+    <div className={css.SignUpPageContainer}>
+      <SignUpForm />
+    </div>
+  );
+};
+
+export default SignUpPage;
