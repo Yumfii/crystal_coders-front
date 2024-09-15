f521de16ade7063d265dc60fd6382406b43db320
Yilchik <yulyayulia2@gmail.com>
2024-09-15 13:40:00 +0200

    SignUpForm

--- src/components/SignUpForm/SignUpForm.jsx
+++ src/components/SignUpForm/SignUpForm.jsx
@@ -2,21 +2,35 @@ import { Field, Form, Formik } from 'formik';
 import { useId } from 'react';
 import * as yup from 'yup';
 import { useDispatch } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 import Logo from 'components/Logo/Logo';
 import css from './SignUpForm.module.css';
+import { registerOperation } from '../../redux/auth/operations';
+
+const emailRegex =
+  /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
 export const validationSchema = yup.object().shape({
   email: yup.string().trim().required('Email is required').matches(emailRegex, {
     message: 'Invalid email.',
   }),
-  password: yup.string().trim().required('Password is required').min(8),
+  password: yup
+    .string()
+    .trim()
+    .required('Password is required')
+    .min(8, 'Password must be at least 8 characters long'),
+  repeatPassword: yup
+    .string()
+    .oneOf([yup.ref('password'), null], 'Passwords must match')
+    .required('Repeat password is required'),
 });
 
 const SignUpForm = () => {
   const dispatch = useDispatch();
   const emailId = useId();
   const passwordId = useId();
+  const repeatPasswordId = useId();
 
   const handleSubmit = (values, actions) => {
     dispatch(registerOperation(values));
@@ -35,9 +49,11 @@ const SignUpForm = () => {
       >
         <Form className={css.SignUpForm}>
           <h2 className={css.SignText}>Sign Up</h2>
+
           <label htmlFor={emailId}>Email </label>
           <Field name="email" id={emailId} placeholder="Enter your email" />
           <br />
+
           <label htmlFor={passwordId}>Password </label>
           <Field
             name="password"
@@ -46,11 +62,12 @@ const SignUpForm = () => {
             placeholder="Enter your password"
           />
           <br />
-          <label htmlFor={passwordId}>Repeat password </label>
+
+          <label htmlFor={repeatPasswordId}>Repeat password </label>
           <Field
             name="repeatPassword"
             type="password"
-            id={passwordId}
+            id={repeatPasswordId}
             placeholder="Repeat password"
           />
           <hr />
