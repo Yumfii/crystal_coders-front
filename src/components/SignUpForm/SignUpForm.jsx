f52b1b5991c70bc5e4db7e28d6aae4c5cfa8b86e
Yumfii <144070576+Yumfii@users.noreply.github.com>
2024-09-16 00:39:39 +0200

    bug fix 1

--- src/components/SignUpForm/SignUpForm.jsx
+++ src/components/SignUpForm/SignUpForm.jsx
@@ -1,4 +1,4 @@
-import { Field, Form, Formik } from 'formik';
+import { Form, Formik } from 'formik';
 import { useId, useState } from 'react';
 import * as yup from 'yup';
 // import { useDispatch } from 'react-redux';
