0192b0a860bf4ff29cbeb67eefdd91378490502e
Yilchik <yulyayulia2@gmail.com>
2024-09-16 23:15:21 +0200

    react-hook-form

--- src/components/SignUpForm/SignUpForm.jsx
+++ src/components/SignUpForm/SignUpForm.jsx
@@ -1,22 +1,20 @@
-import { Form, Formik } from 'formik';
-import { useId, useState } from 'react';
+import { useForm, Controller } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
 import * as yup from 'yup';
-// import { useDispatch } from 'react-redux';
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { FaEye, FaEyeSlash } from 'react-icons/fa';
 
 import Logo from 'components/Logo/Logo';
 import css from './SignUpForm.module.css';
 import { signUp } from 'services/auth';
-// import { registerOperation } from '../../redux/auth/operations';
-
-const emailRegex =
-  /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
 export const validationSchema = yup.object().shape({
-  email: yup.string().trim().required('Email is required').matches(emailRegex, {
-    message: 'Invalid email.',
-  }),
+  email: yup
+    .string()
+    .trim()
+    .required('Email is required')
+    .email('Invalid email format'),
   password: yup
     .string()
     .trim()
@@ -30,112 +28,136 @@ export const validationSchema = yup.object().shape({
 
 const SignUpForm = () => {
   const [passwordVisible, setPasswordVisible] = useState(false);
-  // const dispatch = useDispatch();
-  const emailId = useId();
-  const passwordId = useId();
-  const repeatPasswordId = useId();
+  const [repeatPasswordVisible, setRepeatPasswordVisible] = useState(false);
 
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [repeatPassword, setRepeatPassword] = useState('');
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    defaultValues: {
+      email: '',
+      password: '',
+      repeatPassword: '',
+    },
+    resolver: yupResolver(validationSchema),
+  });
 
-  const handleSubmit = async e => {
-    e.preventDefault();
-    console.log(email, password, repeatPassword);
+  const onSubmit = async data => {
     try {
       const response = await signUp({
-        email: email,
-        password: password,
+        email: data.email,
+        password: data.password,
       });
       console.log(response.data);
-    } catch (er) {
-      console.log(er.message);
+    } catch (err) {
+      console.log(err.message);
     }
   };
+
   return (
     <div className={css.SignUpContainer}>
       <div className={css.logo}>
         <Logo />
       </div>
-      <Formik
-        initialValues={{ email: '', password: '', repeatPassword: '' }}
-        onSubmit={handleSubmit}
-        validationSchema={validationSchema}
-      >
-        <Form className={css.SignUpForm} onSubmit={handleSubmit}>
-          <h2 className={css.SignText}>Sign Up</h2>
+      <form className={css.SignUpForm} onSubmit={handleSubmit(onSubmit)}>
+        <h2 className={css.SignText}>Sign Up</h2>
+        <div className={css.inputDiv}>
+          <label>Email</label>
+          <Controller
+            name="email"
+            control={control}
+            render={({ field }) => (
+              <input
+                {...field}
+                placeholder="Enter your email"
+                className={`${css.input} ${errors.email ? css.inputError : ''}`}
+              />
+            )}
+          />
+          {errors.email && <p className={css.error}>{errors.email.message}</p>}
+        </div>
 
-          <div className={css.inputDiv}>
-            <label htmlFor={emailId}>Email </label>
-            <input
-              name="email"
-              // id={emailId}
-              onChange={e => {
-                setEmail(e.target.value);
-              }}
-              placeholder="Enter your email"
-              className={css.input}
-              value={email}
+        <div className={css.inputDiv}>
+          <label>Password</label>
+          <div className={css.wrapPass}>
+            <Controller
+              name="password"
+              control={control}
+              render={({ field }) => (
+                <input
+                  {...field}
+                  type={passwordVisible ? 'text' : 'password'}
+                  placeholder="Enter your password"
+                  className={`${css.input} ${
+                    errors.password ? css.inputError : ''
+                  }`}
+                />
+              )}
             />
+            <button
+              type="button"
+              className={css.toggle}
+              onClick={() => setPasswordVisible(!passwordVisible)}
+            >
+              {passwordVisible ? <FaEye /> : <FaEyeSlash />}
+            </button>
           </div>
+          {errors.password && (
+            <p className={css.error}>{errors.password.message}</p>
+          )}
+        </div>
 
-          <div className={css.inputDiv}>
-            <label htmlFor={passwordId}>Password </label>
-            <div className={css.wrapPass}>
-              <input
-                name="password"
-                type="password"
-                // id={passwordId}
-                onChange={e => {
-                  setPassword(e.target.value);
-                }}
-                placeholder="Enter your password"
-                className={css.input}
-                value={password}
-              />
-              <button
-                type="button"
-                className={css.toggle}
-                onClick={() => setPasswordVisible(!passwordVisible)}
-              >
-                {passwordVisible ? <FaEye /> : <FaEyeSlash />}
-              </button>
-            </div>
+        <div className={css.inputDiv}>
+          {/* <label htmlFor={repeatPasswordId}>Repeat password </label> */}
+          <label>Repeat password</label>
+          <div className={css.wrapPass}>
+            <Controller
+              name="repeatPassword"
+              control={control}
+              render={({ field }) => (
+                <input
+                  {...field}
+                  type={repeatPasswordVisible ? 'text' : 'password'}
+                  // <input
+                  //   name="repeatPassword"
+                  //   type="password"
+                  //   // id={repeatPasswordId}
+                  //   onChange={e => {
+                  //     setRepeatPassword(e.target.value);
+                  //   }}
+                  placeholder="Repeat password"
+                  className={`${css.input} ${
+                    errors.repeatPassword ? css.inputError : ''
+                  }`}
+                  // value={repeatPassword}
+                />
+              )}
+            />
+            <button
+              type="button"
+              className={css.toggle}
+              onClick={() => setRepeatPasswordVisible(!repeatPasswordVisible)}
+            >
+              {repeatPasswordVisible ? <FaEye /> : <FaEyeSlash />}
+            </button>
           </div>
-          <div className={css.inputDiv}>
-            <label htmlFor={repeatPasswordId}>Repeat password </label>
-            <div className={css.wrapPass}>
-              <input
-                name="repeatPassword"
-                type="password"
-                // id={repeatPasswordId}
-                onChange={e => {
-                  setRepeatPassword(e.target.value);
-                }}
-                placeholder="Repeat password"
-                className={css.input}
-                value={repeatPassword}
-              />
-              <button
-                type="button"
-                className={css.toggle}
-                onClick={() => setPasswordVisible(!passwordVisible)}
-              >
-                {passwordVisible ? <FaEye /> : <FaEyeSlash />}
-              </button>
-            </div>
-          </div>
-          <button type="submit" className={css.btn}>
-            Sign Up
-          </button>
-          <p className={css.text}>
-            Already have account?{' '}
-            <Link to="/signin" className={css.linkText}>
-              Sign In
-            </Link>
-          </p>
-        </Form>
-      </Formik>
+          {errors.repeatPassword && (
+            <p className={css.error}>{errors.repeatPassword.message}</p>
+          )}
+        </div>
+        <button type="submit" className={css.btn}>
+          Sign Up
+        </button>
+        <p className={css.text}>
+          Already have account?{' '}
+          <Link to="/signin" className={css.linkText}>
+            Sign In
+          </Link>
+        </p>
+        {/* </Form>
+      </Formik> */}
+      </form>
     </div>
   );
 };
