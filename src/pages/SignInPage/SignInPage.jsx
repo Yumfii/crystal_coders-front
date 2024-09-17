798bdf3bf933ff1b723fc8697a336f23536759fe
Lev <leozolotukhin@gmail.com>
2024-09-17 22:05:40 +0200

    signin 2
    redux
    googlebtn hover
    singin page route

--- src/pages/SignInPage/SignInPage.jsx
+++ src/pages/SignInPage/SignInPage.jsx
@@ -2,16 +2,22 @@ import css from './SignInPage.module.css'; // Import CSS as a module
 
 import AdvantagesSection from '../../components/AdvantagesSection/AdvantagesSection';
 import SignInForm from 'components/SignInForm/SignInForm';
+import { useSelector } from 'react-redux';
+import { selectIsLoggedIn } from '../../redux/auth/selectors';
 
+import { useNavigate } from 'react-router-dom';
 const SignInPage = () => {
+  const isLoggedIn = useSelector(selectIsLoggedIn);
+  const navigate = useNavigate();
   return (
     <div className={css.main}>
       <SignInForm />
       <div className={css.advantagesSection}>
         <AdvantagesSection />
       </div>
+      {isLoggedIn ? navigate('/') : null}
     </div>
-  );
+  ); // замінити на watertracker route
 };
 
 export default SignInPage;
