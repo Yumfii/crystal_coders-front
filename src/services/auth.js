3fb901fe85f525f57230aed37e31757aa86bcc9a
zero <sanyafullgit@gmail.com>
2024-09-18 13:31:07 +0300

    fix navigate error

--- src/services/auth.js
+++ src/services/auth.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export async function signUp(body) {
   const response = await axios.post(
-    'https://crystal-coders-back.onrender.com/register',
+    'https://crystal-coders-back.onrender.com/auth/register',
     body
   );
 
