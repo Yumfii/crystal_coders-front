074aace8c0278f572c9499adf2a990f47beef556
Roman Vashchenko <roman1991dp@gmail.com>
2024-09-19 23:44:38 +0300

    update

--- src/components/WaterDetailedInfo/WaterDetailedInfo.jsx
+++ src/components/WaterDetailedInfo/WaterDetailedInfo.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import css from './WaterDetailedInfo.module.css';
 
 const WaterDetailedInfo = () => {
-  return <div></div>;
+  return (
+    <section className={css.section}>
+      <div className={css.container}></div>
+    </section>
+  );
 };
 
 export default WaterDetailedInfo;
