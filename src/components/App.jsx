642d04eec390064965d826a965ded7700c40a973
Yumfii <144070576+Yumfii@users.noreply.github.com>
2024-09-19 22:22:29 +0200

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,6 +2,8 @@ import HomePage from 'pages/HomePage/HomePage';
 import SignInPage from 'pages/SignInPage/SignInPage';
 import SignUpPage from 'pages/SignUpPage/SignUpPage';
 import TrackerPage from 'pages/TrackerPage/TrackerPage';
+import UserSettingsModalT from 'pages/UserSettingsModal/UserSettingModalT';
+// import UserSettingsModalT from 'pages/UserSettingsModal/UserSettingsModalT';
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 
@@ -13,6 +15,8 @@ export const App = () => {
       <Route path="/signin" element={<SignInPage />} />
       <Route path="/signup" element={<SignUpPage />} />
       <Route path="/tracker" element={<TrackerPage />} />
+      <Route path="/userSetting" element={<UserSettingsModalT />} />
+      <Route path="/waterform" element={<UserSettingsModalT />} />
     </Routes>
   );
 };
