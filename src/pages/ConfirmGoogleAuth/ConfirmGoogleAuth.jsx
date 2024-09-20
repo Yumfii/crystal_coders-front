d90c45af8f27c04d8f36379929bb52d716925daf
zero <sanyafullgit@gmail.com>
2024-09-20 17:13:07 +0300

    Update ConfirmGoogleAuth.jsx

--- src/pages/ConfirmGoogleAuth/ConfirmGoogleAuth.jsx
+++ src/pages/ConfirmGoogleAuth/ConfirmGoogleAuth.jsx
@@ -8,26 +8,21 @@ const ConfirmGoogleAuth = () => {
   useEffect(() => {
     const urlParams = new URLSearchParams(window.location.search);
     const code = urlParams.get('code');
-    console.log(code);
 
     if (code) {
-      axios.post('https://crystal-coders-back.onrender.com/auth/confirm-oauth', {
-        code: code
-      })
-      .then(() => {
-        navigate('/tracker');
-      })
-      .catch(error => {
-        console.error("Error during Google OAuth callback:", error);
-      });
+      axios.post('https://crystal-coders-back.onrender.com/auth/confirm-oauth', { code })
+        .then(response => {
+          const accessToken = response.data.data.accessToken;
+          localStorage.setItem('accessToken', accessToken);
+          navigate('/tracker');
+        })
+        .catch(error => {
+          console.error("Error during Google OAuth callback:", error);
+        });
     }
   }, [navigate]);
 
-  return (
-    <div>
-      <h1>Authenticating...</h1>
-    </div>
-  );
+  return <div><h1>Authenticating...</h1></div>;
 };
 
 export default ConfirmGoogleAuth;
