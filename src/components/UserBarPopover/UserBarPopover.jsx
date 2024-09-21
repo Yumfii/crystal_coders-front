f8bc0cffacf36c84b60810b5086dcc4e29000748
mariavsl518 <mariavsl518@gmail.com>
2024-09-21 13:56:56 +0200

    tracker page styles fixed

--- src/components/UserBarPopover/UserBarPopover.jsx
+++ src/components/UserBarPopover/UserBarPopover.jsx
@@ -1,15 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 import CSS from './UserBarPopover.module.css'
 import { TbSettings } from "react-icons/tb";
 import { LuLogOut } from "react-icons/lu";
 
 const UserBarPopover = () => {
+  const [openModa, setOpenModal] = useState(false)
 
+  const isModalOpen = () => {
+    setOpenModal(true)
+  }
 
   return (
     <div className={CSS.popoverBlock}>
       <button type='button' className={`${CSS.popoverBtn}`}>
-        <TbSettings className={CSS.settingsIcon} />
+        <TbSettings className={CSS.settingsIcon}
+          onClick={isModalOpen}/>
         Settings
       </button>
       <button type='button' className={`${CSS.popoverBtn} ${CSS.transparentBtn}`}>
