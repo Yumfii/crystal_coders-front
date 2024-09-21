de3296bcc29f95103a2d84bc03808f6dadb301db
Maria Ensari <149113366+marotr@users.noreply.github.com>
2024-09-21 10:19:58 +0300

    tour update

--- src/pages/TrackerPage/TrackerPage.jsx
+++ src/pages/TrackerPage/TrackerPage.jsx
@@ -1,50 +1,31 @@
+import React, { useEffect } from 'react';
 import WaterDetailedInfo from 'components/WaterDetailedInfo/WaterDetailedInfo';
 import WaterMainInfo from 'components/WaterMainInfo/WaterMainInfo';
+import AddWaterBtn from 'components/AddWaterBtn/AddWaterBtn';
 import css from './TrackerPage.module.css';
-import React, { useEffect, useState } from 'react';
-import Tour from '@reactour/tour';
+import { useTour } from '@reactour/tour';
 
 const TrackerPage = () => {
-  const [isTourOpen, setIsTourOpen] = useState(false);
-const steps = [
-  {selector:'.dailyNorma',
-    content: 'This is your daily norma for water intake',
-    position:'right',
+  const { setIsOpen } = useTour();
 
-  },
-  {selector: '.waterProgressBar',
-    content:'This is how much water you have had today',
-    position:'right',
-  },
 
- { selector: 'styleBtn',
-  content:'Here you can add water',
-  position:'left',
- }
-];
+  useEffect(() => {
+    const hasSeenTour = localStorage.getItem('hasSeenTrackerTour');
+    if (!hasSeenTour) {
+      setIsOpen(true);
+    }
+  }, [setIsOpen]);
 
-useEffect(() => {
-  const hasSeenTour = localStorage.getItem('hasSeenTrackerTour');
-
-  if (!hasSeenTour) {
-    setIsTourOpen(true);
-  }
-}, []);
-
-const closeTour = () => {
-  setIsTourOpen(false);
-  localStorage.setItem('hasSeenTrackerTour', 'true');
-};
+  const closeTour = () => {
+    setIsOpen(false);
+    localStorage.setItem('hasSeenTrackerTour', 'true');
+  };
 
   return (
     <div className={css.container}>
-        <Tour
-        steps={steps}
-        isOpen={isTourOpen}
-        onClose={closeTour}
-      />
       <WaterMainInfo />
       <WaterDetailedInfo />
+      <AddWaterBtn variant="primary" />
     </div>
   );
 };
