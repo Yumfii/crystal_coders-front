fdd310513fc98499aae7ca8e280dad8b750143f4
Maryna Binko <benyhorses@hotmail.com>
2024-09-22 17:16:16 +0100

    dailyInfoModaalEdit

--- src/components/DailyInfo/DailyInfo.jsx
+++ src/components/DailyInfo/DailyInfo.jsx
@@ -2,15 +2,34 @@ import AddWaterBtn from 'components/AddWaterBtn/AddWaterBtn';
 import ChooseDate from 'components/ChooseDate/ChooseDate';
 import WaterList from 'components/WaterList/WaterList';
 import css from './DailyInfo.module.css';
+import React, { useState } from 'react';
+import Modal from 'components/Modal/Modal';
+import WaterModal from 'components/WaterModal/WaterModal';
 
 const DailyInfo = ({ selectedDate }) => {
+  const [isModalOpen, setModalOpen] = useState(false);
+
+  const handleOpenModal = () => {
+    setModalOpen(true);
+  };
+
+  const handleCloseModal = () => {
+    setModalOpen(false);
+  };
+
   return (
     <div>
       <div className={css.wrapper}>
         <ChooseDate selectedDate={selectedDate} />
-        <AddWaterBtn variant="secondary" />
+        <AddWaterBtn variant="secondary" onClick={handleOpenModal} />
       </div>
       <WaterList />
+
+      {isModalOpen && (
+        <Modal isOpen={isModalOpen} onClose={handleCloseModal}>
+          <WaterModal operationType="add" onClose={handleCloseModal} />
+        </Modal>
+      )}
     </div>
   );
 };
