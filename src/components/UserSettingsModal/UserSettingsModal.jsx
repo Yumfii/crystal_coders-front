e8fd3120b38ce29ee8dfa7c25664ae61091aa04f
Maria Ensari <149113366+marotr@users.noreply.github.com>
2024-09-23 10:43:17 +0300

    updated json

--- src/components/UserSettingsModal/UserSettingsModal.jsx
+++ src/components/UserSettingsModal/UserSettingsModal.jsx
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import React from 'react'
 import UserSettingsForm from '../UserSettingsForm/UserSettingsForm'
 import CSS from './UserSettingsModal.module.css'
@@ -14,3 +15,42 @@ const UserSettingsModal = () => {
 }
 
 export default UserSettingsModal
+=======
+import React, { useEffect } from 'react';
+import UserSettingsForm from '../UserSettingsForm/UserSettingsForm';
+import CSS from './UserSettingsModal.module.css';
+import { useTour } from '@reactour/tour';
+import { stepsModal } from 'components/stepsModal';
+
+
+
+const UserSettingsModal = () => {
+  const { setIsOpen, setSteps, close } = useTour();
+
+  useEffect(() => {
+    setSteps(stepsModal);
+    const hasSeenModalTour = localStorage.getItem('hasSeenModalTour');
+    if (!hasSeenModalTour) {
+      const timer = setTimeout(() => {
+        setIsOpen(true);
+      }, 500);
+
+      return () => clearTimeout(timer);
+    }
+  }, [setIsOpen, setSteps]);
+
+  const handleTourComplete = () => {
+    localStorage.setItem('hasSeenModalTour', 'true');
+    close();
+  };
+
+  return (
+    <div className={CSS.modalContainer}>
+      <h2 className={CSS.settingsHeader}>Settings</h2>
+      <UserSettingsForm  onTourComplete={handleTourComplete}/>
+    </div>
+  );
+};
+
+export default UserSettingsModal;
+>>>>>>> Stashed changes
--- src/pages/TrackerPage/TrackerPage.jsx
+++ src/pages/TrackerPage/TrackerPage.jsx
@@ -1,10 +1,22 @@
+<<<<<<< Updated upstream
 import WaterDetailedInfo from '../../components/WaterDetailedInfo/WaterDetailedInfo';
 import WaterMainInfo from '../../components/WaterMainInfo/WaterMainInfo';
 import css from './TrackerPage.module.css';
 import React, { useEffect } from 'react';
+=======
+import React, { useEffect } from 'react';
+import css from './TrackerPage.module.css';
+import { useTour } from '@reactour/tour';
+
+>>>>>>> Stashed changes
 import { selectUser } from '../../redux/auth/selectors.js';
 import { useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
+import WaterDetailedInfo from 'components/WaterDetailedInfo/WaterDetailedInfo';
+import WaterMainInfo from 'components/WaterMainInfo/WaterMainInfo';
+import AddWaterBtn from 'components/AddWaterBtn/AddWaterBtn';
+import { steps } from 'components/steps';
+
 
 const TrackerPage = () => {
   const selector = useSelector(selectUser);
