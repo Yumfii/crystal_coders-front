3d7c7010931ee0367f4798156b2f2f8b8f569a3d
Maria Ensari <149113366+marotr@users.noreply.github.com>
2024-09-25 06:41:33 +0300

    Update MonthInfo.jsx

--- src/components/MonthInfo/MonthInfo.jsx
+++ src/components/MonthInfo/MonthInfo.jsx
@@ -3,15 +3,23 @@ import Calendar from '../../components/Calendar/Calendar';
 import CalendarPagination from '../../components/CalendarPagination/CalendarPagination';
 import { addMonths, subMonths } from 'date-fns';
 import css from './MonthInfo.module.css';
-import axios from 'axios';
 import { useDispatch, useSelector } from 'react-redux';
+import {
+  fetchWaterConsumptionForMonth,
+} from 'redux/water/operations';
 
 const MonthInfo = ({ selectedDate, setSelectedDate }) => {
   const dispatch = useDispatch();
   // !! Check how to get data about percentage of the water
-  const [waterData, loading, error] = useSelector(state => state.water);
+  const [waterConsumption, loading, error] = useSelector(state => state.water);
+  useEffect(() => {
+    if (selectedDate) {
+      const month = selectedDate.getMonth() + 1;
+      dispatch(fetchWaterConsumptionForMonth(month));
+    }
+  }, [selectedDate, dispatch]);
 
-    // !! Check the part above
+  // !! Check the part above
 
   const nextMonth = () => {
     setSelectedDate(prevDate => addMonths(prevDate, 1));
@@ -20,8 +28,6 @@ const MonthInfo = ({ selectedDate, setSelectedDate }) => {
   const previousMonth = () => {
     setSelectedDate(prevDate => subMonths(prevDate, 1));
   };
-
-
 
   return (
     <div className={`${css.monthlyInfo} monthlyInfo`}>
@@ -33,9 +39,11 @@ const MonthInfo = ({ selectedDate, setSelectedDate }) => {
           nextMonth={nextMonth}
         />
       </div>
-      <Calendar selectedDate={selectedDate} setSelectedDate={setSelectedDate}
-       waterData={waterData}
-       />
+      <Calendar
+        selectedDate={selectedDate}
+        setSelectedDate={setSelectedDate}
+        waterData={waterConsumption}
+      />
     </div>
   );
 };
